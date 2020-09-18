29568ec63d5df42d3bbfb1a6b782d0846db74877
Lova Wins <lovaunger@gmail.com>
2020-09-18 22:47:49 +0200

    docker stuff

--- src/App.vue
+++ src/App.vue
@@ -10,23 +10,18 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Muli, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: hotpink;
 }
 
-#nav {
-  padding: 30px;
+a {
+  color:hotpink;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+a:hover {
+ font-weight: bold;
 }
 </style>
