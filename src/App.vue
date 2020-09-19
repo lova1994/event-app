0305401edb70b92ec0bfe0660f051bf416c4c143
Lova Wins <lovaunger@gmail.com>
2020-09-19 16:00:24 +0200

    forgot to add tests

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
   <div id="app">
+     <Header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+   
   </div>
 </template>
+
+<script>
+import Header from './components/Header.vue';
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+  props: {
+    events: Object
+  },
+  data: () => ({
+    title: "Event Finder",
+    events: [
+      { tag: "Adventure", id: 1, date: "Thu, oct 1, 18:00", title: "Hot Air Balloon Ride", description: "Group Adventure, LA", img: "https://images.pexels.com/photos/670061/pexels-photo-670061.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+      },
+         { tag: "Art", id: 2, date: "Tue, sep 2, 12:00", title: "Street Art Guide", description: "Artistic Event, LA", img: "https://images.pexels.com/photos/959314/pexels-photo-959314.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+      },
+       { tag: "Nature", id: 3, date: "Tue, oct 10, 09:00", title: "High Mountains Hike", description: "Nature Event, LA", img: "https://images.pexels.com/photos/2356059/pexels-photo-2356059.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+      },
+
+
+    ]
+  }),
+ 
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -25,3 +54,5 @@ a:hover {
  font-weight: bold;
 }
 </style>
+
+
