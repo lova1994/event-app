dd00636fcd3b802fc2b229e1d3ca7cc9f0fd1331
Lova Wins <lovaunger@gmail.com>
2020-09-22 00:45:44 +0200

    testing

---
+++ tests/unit/events.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils'
+import Events from '@/components/Events.vue'
+
+describe('Events.vue', () => {
+  test('snapshot', () => {
+      const wrapper = shallowMount(Events)
+      expect(wrapper).toMatchSnapshot()
+    
+ 
+
+  })
+})
