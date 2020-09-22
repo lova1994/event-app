65a5af2e53ceeac9aedad31b4bc1e143c402eccd
Lova Wins <lovaunger@gmail.com>
2020-09-22 14:30:22 +0200

    added profile page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Profile from "../views/Profile.vue";
+
 
 Vue.use(VueRouter)
 
@@ -17,7 +19,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+    // add the route to the /profile component
+    {
+      path: "/profile",
+      name: "profile",
+      component: Profile
+    }
 ]
 
 const router = new VueRouter({
